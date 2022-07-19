66df387ab4c0e00de88a4408f81920df13d70a54
ASUS <ASUS@DESKTOP-TESLJ9O>
2022-07-19 11:19:56 +0530

    try

---
+++ Spring/src/client.java
@@ -0,0 +1,16 @@
+
+public class client {
+
+	public static void main(String[] args) {
+		Employee e =new Employee();
+		e.setEid(101);
+		e.setEname("John");
+		e.setEsalary(20000);
+		
+		System.out.println(e);
+		
+		
+
+	}
+
+}
